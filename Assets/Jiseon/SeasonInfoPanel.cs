075faf13f8868c4b267638363ee7d12a57d946bb
DoJiSeon <djs6620@gmail.com>
2025-05-10 06:50:36 +0900

    [기능 수정]2ROOM 수정중

--- Assets/Jiseon/SeasonInfoPanel.cs
+++ Assets/Jiseon/SeasonInfoPanel.cs
@@ -11,17 +11,23 @@ public class SeasonInfoPanel : MonoBehaviour
     public Image seasonImage;
     public VideoPlayer seasonVideo;
 
+    private Dictionary<string, string> displayNames = new Dictionary<string, string>()
+    {
+        {"입춘", "입춘 (立春)"},
+        {"우수", "우수 (雨水)"}
+        // ...
+    };
+
     private Dictionary<string, string> descriptions = new Dictionary<string, string>()
     {
-        {"입춘", "입춘은 봄의 시작을 알리는 절기입니다."},
-        {"우수", "우수는 눈이 녹고 봄비가 내리는 절기입니다."},
+        {"입춘", "입춘은 봄의 시작을 알리는 절기로, 농촌에서는 밭을 갈고 온실을 점검하며 농사 준비를 시작합니다. 감자나 조생종 작물의 씨앗을 선별하고 모종을 기르는 작업이 본격적으로 이루어집니다."},
+        {"우수", "우수는 눈이 녹고 비가 내려 땅이 풀리는 절기로, 농사 기반이 본격적으로 마련되는 시기입니다. 논밭을 갈고 감자, 마늘, 양파 등 봄 작물의 파종 준비와 월동 작물의 생육 관리가 이루어집니다."}
         // ...
     };
 
     private Dictionary<string, Sprite> images = new Dictionary<string, Sprite>();
     private Dictionary<string, VideoClip> videos = new Dictionary<string, VideoClip>();
 
-    // 이미지, 비디오 등록 (Start에서 등록하거나 외부에서 불러오기 가능)
     public Sprite ipchunImage;
     public Sprite usuImage;
     //public VideoClip ipchunVideo;
@@ -35,20 +41,24 @@ public class SeasonInfoPanel : MonoBehaviour
         //videos["우수"] = usuVideo;
     }
 
-    public void ShowSeasonInfo(string seasonName)
+    public void ShowSeasonInfo(string seasonKey)
     {
-        titleText.text = seasonName;
+        // 제목 표시 (한자 포함)
+        if (displayNames.ContainsKey(seasonKey))
+            titleText.text = displayNames[seasonKey];
+        else
+            titleText.text = seasonKey;
 
         // 텍스트 설명
-        if (descriptions.ContainsKey(seasonName))
-            descText.text = descriptions[seasonName];
+        if (descriptions.ContainsKey(seasonKey))
+            descText.text = descriptions[seasonKey];
         else
             descText.text = "설명이 없습니다.";
 
         // 이미지 표시
-        if (images.ContainsKey(seasonName))
+        if (images.ContainsKey(seasonKey))
         {
-            seasonImage.sprite = images[seasonName];
+            seasonImage.sprite = images[seasonKey];
             seasonImage.gameObject.SetActive(true);
         }
         else
@@ -56,10 +66,10 @@ public class SeasonInfoPanel : MonoBehaviour
             seasonImage.gameObject.SetActive(false);
         }
 
-        // 영상 재생
-        //if (videos.ContainsKey(seasonName))
+        // 영상 재생 (주석 유지 시 생략)
+        //if (videos.ContainsKey(seasonKey))
         //{
-        //    seasonVideo.clip = videos[seasonName];
+        //    seasonVideo.clip = videos[seasonKey];
         //    seasonVideo.Play();
         //}
         //else
