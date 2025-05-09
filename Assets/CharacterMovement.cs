a4bbe44fe4d965d4f0df0935951e9e354e3f88f2
tndmstla <jineung78@gmail.com>
2025-05-09 19:17:50 +0900

    player movement

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 mov = new Vector3(h, 0, v);
+        this.transform.Translate(mov * Time.deltaTime * speed);
+    }
+}
